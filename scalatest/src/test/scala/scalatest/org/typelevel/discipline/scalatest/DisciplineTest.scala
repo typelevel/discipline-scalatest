2a55ba6b660a13874a3f7088942675d7462adad9
Ross A. Baker <ross@rossabaker.com>
2019-08-02 22:02:59 -0400

    Add DummyLaws to test

--- scalatest/src/test/scala/scalatest/org/typelevel/discipline/scalatest/DisciplineTest.scala
+++ scalatest/src/test/scala/scalatest/org/typelevel/discipline/scalatest/DisciplineTest.scala
@@ -4,5 +4,5 @@ package scalatest
 import org.scalatest.funsuite.AnyFunSuiteLike
 
 class LawTests extends AnyFunSuiteLike with Discipline {
-  // checkAll("Int", RingLaws.ring)
+  checkAll("Dummy", DummyLaws.dummy)
 }
