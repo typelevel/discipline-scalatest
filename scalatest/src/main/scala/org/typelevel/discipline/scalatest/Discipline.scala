e0d2262bad4c9767fd93d2b81626275f2b07a945
Travis Brown <travisrobertbrown@gmail.com>
2020-01-17 04:59:41 -0600

    Convert configuration for Checkers

--- scalatest/src/main/scala/org/typelevel/discipline/scalatest/Discipline.scala
+++ scalatest/src/main/scala/org/typelevel/discipline/scalatest/Discipline.scala
@@ -10,6 +10,26 @@ import org.scalatest.prop.Configuration
 import org.scalatestplus.scalacheck.Checkers
 
 trait Discipline { self: Configuration =>
+
+  /**
+   * Convert from our configuration type to the one required by `Checkers`.
+   *
+   * We don't extend `Checkers` because we want to leave the user as much
+   * control as possible over available testing styles. Unfortunately we need
+   * this conversion because ScalaTest defines `PropertyCheckConfiguration`
+   * as a case class in the `Configuration` trait.
+   */
+  final protected[this] def convertConfiguration(
+    config: PropertyCheckConfiguration
+  ): Checkers.PropertyCheckConfiguration =
+    Checkers.PropertyCheckConfiguration(
+      config.minSuccessful,
+      config.maxDiscardedFactor,
+      config.minSize,
+      config.sizeRange,
+      config.workers
+    )
+
   def checkAll(name: String, ruleSet: Laws#RuleSet)(implicit config: PropertyCheckConfiguration,
                                                     prettifier: Prettifier,
                                                     pos: Position): Unit
@@ -21,10 +41,10 @@ trait FlatSpecDiscipline extends Discipline { self: AnyFlatSpecLike with Configu
                                                           pos: Position): Unit =
     ruleSet.all.properties match {
       case first +: rest =>
-        name should first._1 in Checkers.check(first._2)
+        name should first._1 in Checkers.check(first._2)(convertConfiguration(config), prettifier, pos)
 
         for ((id, prop) <- rest)
-          it should id in Checkers.check(prop)
+          it should id in Checkers.check(prop)(convertConfiguration(config), prettifier, pos)
     }
 }
 
@@ -35,7 +55,7 @@ trait FunSpecDiscipline extends Discipline { self: AnyFunSpecLike with Configura
     describe(name) {
       for ((id, prop) <- ruleSet.all.properties)
         it(id) {
-          Checkers.check(prop)
+          Checkers.check(prop)(convertConfiguration(config), prettifier, pos)
         }
     }
 }
@@ -46,6 +66,6 @@ trait FunSuiteDiscipline extends Discipline { self: AnyFunSuiteLike with Configu
                                                           pos: Position): Unit =
     for ((id, prop) <- ruleSet.all.properties)
       test(s"${name}.${id}") {
-        Checkers.check(prop)
+        Checkers.check(prop)(convertConfiguration(config), prettifier, pos)
       }
 }
