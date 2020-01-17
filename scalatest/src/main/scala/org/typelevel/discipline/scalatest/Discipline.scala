967be4edaf1cfb4484882e622162a3475bc9eafa
Travis Brown <travisrobertbrown@gmail.com>
2020-01-17 03:53:26 -0600

    Improve position reporting

--- scalatest/src/main/scala/org/typelevel/discipline/scalatest/Discipline.scala
+++ scalatest/src/main/scala/org/typelevel/discipline/scalatest/Discipline.scala
@@ -1,17 +1,18 @@
 package org.typelevel.discipline
 package scalatest
 
+import org.scalactic.source.Position
 import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.funspec.AnyFunSpecLike
 import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatestplus.scalacheck.Checkers
 
 trait Discipline {
-  def checkAll(name: String, ruleSet: Laws#RuleSet): Unit
+  def checkAll(name: String, ruleSet: Laws#RuleSet)(implicit pos: Position): Unit
 }
 
 trait FlatSpecDiscipline extends Discipline { self: AnyFlatSpecLike =>
-  final def checkAll(name: String, ruleSet: Laws#RuleSet): Unit =
+  final def checkAll(name: String, ruleSet: Laws#RuleSet)(implicit pos: Position): Unit =
     ruleSet.all.properties match {
       case first +: rest =>
         name should first._1 in Checkers.check(first._2)
@@ -22,7 +23,7 @@ trait FlatSpecDiscipline extends Discipline { self: AnyFlatSpecLike =>
 }
 
 trait FunSpecDiscipline extends Discipline { self: AnyFunSpecLike =>
-  final def checkAll(name: String, ruleSet: Laws#RuleSet): Unit =
+  final def checkAll(name: String, ruleSet: Laws#RuleSet)(implicit pos: Position): Unit =
     describe(name) {
       for ((id, prop) <- ruleSet.all.properties)
         it(id) {
@@ -32,7 +33,7 @@ trait FunSpecDiscipline extends Discipline { self: AnyFunSpecLike =>
 }
 
 trait FunSuiteDiscipline extends Discipline { self: AnyFunSuiteLike =>
-  final def checkAll(name: String, ruleSet: Laws#RuleSet): Unit =
+  final def checkAll(name: String, ruleSet: Laws#RuleSet)(implicit pos: Position): Unit =
     for ((id, prop) <- ruleSet.all.properties)
       test(s"${name}.${id}") {
         Checkers.check(prop)
